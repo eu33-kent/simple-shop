e864fd035dbe2f86b61449d3a9de2e5cf7a60c67
eu33 <eu33@kent.ac.uk>
2021-05-09 16:59:28 +0100

    prevented orders from having no associated product or user ids, ensured that the name of a product (trimmed) is at least 3 characters, added an image path attribute to products, and added some example products.

--- Database/initialise.sql
+++ Database/initialise.sql
@@ -20,16 +20,26 @@ CREATE TABLE logs (
 );
 CREATE TABLE products (
 	pid INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NOT NULL CHECK (price > 0)
+    name VARCHAR(50) NOT NULL CHECK (LENGTH(TRIM(name)) > 2),
+    price DECIMAL(10,2) NOT NULL CHECK (price > 0),
+    imgPath VARCHAR(255)
 );
 CREATE TABLE orders (
 	oid INT PRIMARY KEY AUTO_INCREMENT,
-    uid INT,
-    pid INT,
+    uid INT NOT NULL,
+    pid INT NOT NULL,
     FOREIGN KEY (uid) REFERENCES users(uid),
     FOREIGN KEY (pid) REFERENCES products(pid)
 );
 
 INSERT INTO users (login, passHash, firstName, lastName)
 VALUES ('tricksy.', 'fba14b5274133422ebbe7c5c8496f47a', 'Elliot', 'Ursell');
+
+INSERT INTO products (name, price, imgPath)
+VALUES ('NoVidIdea Graphics Card', 9999.98, 'https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3090.png');
+INSERT INTO products (name, price, imgPath)
+VALUES ('Pear aPhone 23XSI', 1452.71, 'https://ih1.redbubble.net/image.1027498293.4407/flat,750x1000,075,f.jpg');
+INSERT INTO products (name, price, imgPath)
+VALUES ('Freeddo', 0.05, 'https://upload.wikimedia.org/wikipedia/en/a/a1/Freddoaus.jpg');
+INSERT INTO products (name, price, imgPath)
+VALUES ('Nokeya Brick', 4.5, 'https://i.redd.it/3xzvk9eg7aj11.jpg');
