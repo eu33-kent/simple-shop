f92c7b7c7d13cac058b3dd5b47722a9e0a7dc079
eu33 <eu33@kent.ac.uk>
2021-05-09 23:53:59 +0100

    applied constraints refactoring to the Login GUI.

--- Java/src/gui/Login.java
+++ Java/src/gui/Login.java
@@ -8,7 +8,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JTextField;
 
 import functions.Databaser;
@@ -80,47 +79,32 @@ public class Login {
 		
 		JLabel lblLogin = new JLabel("Login to simple-shop");
 		lblLogin.setFont(Globals.font(24));
-		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
-		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLogin.gridx = 1;
-		gbc_lblLogin.gridy = 0;
+		GridBagConstraints gbc_lblLogin = Globals.gbc(1,0);
 		frmLogin.getContentPane().add(lblLogin, gbc_lblLogin);
 		
 		JLabel lblUsername = new JLabel("Username");
 		lblUsername.setFont(Globals.font);
-		GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+		GridBagConstraints gbc_lblUsername = Globals.gbc(1,2);
 		gbc_lblUsername.anchor = GridBagConstraints.WEST;
-		gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
-		gbc_lblUsername.gridx = 1;
-		gbc_lblUsername.gridy = 2;
 		frmLogin.getContentPane().add(lblUsername, gbc_lblUsername);
 		
 		txtUsername = new JTextField();
 		txtUsername.setFont(Globals.font);
-		GridBagConstraints gbc_txtUsername = new GridBagConstraints();
+		GridBagConstraints gbc_txtUsername = Globals.gbc(1,3);
 		gbc_txtUsername.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtUsername.insets = new Insets(0, 0, 5, 5);
-		gbc_txtUsername.gridx = 1;
-		gbc_txtUsername.gridy = 3;
 		frmLogin.getContentPane().add(txtUsername, gbc_txtUsername);
 		txtUsername.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setFont(Globals.font);
-		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		GridBagConstraints gbc_lblPassword = Globals.gbc(1,5);
 		gbc_lblPassword.anchor = GridBagConstraints.WEST;
-		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPassword.gridx = 1;
-		gbc_lblPassword.gridy = 5;
 		frmLogin.getContentPane().add(lblPassword, gbc_lblPassword);
 		
 		txtPassword = new JPasswordField();
 		txtPassword.setFont(Globals.font);
-		GridBagConstraints gbc_txtPassword = new GridBagConstraints();
-		gbc_txtPassword.insets = new Insets(0, 0, 5, 5);
+		GridBagConstraints gbc_txtPassword = Globals.gbc(1,6);
 		gbc_txtPassword.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtPassword.gridx = 1;
-		gbc_txtPassword.gridy = 6;
 		frmLogin.getContentPane().add(txtPassword, gbc_txtPassword);
 		txtPassword.setColumns(10);
 		
@@ -141,18 +125,12 @@ public class Login {
 			}
 		});
 		btnLogin.setFont(Globals.font);
-		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
-		gbc_btnLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_btnLogin.gridx = 1;
-		gbc_btnLogin.gridy = 8;
+		GridBagConstraints gbc_btnLogin = Globals.gbc(1,8);
 		frmLogin.getContentPane().add(btnLogin, gbc_btnLogin);
 		
 		JLabel lblRegister = new JLabel("Don't have an account?");
 		lblRegister.setFont(Globals.font);
-		GridBagConstraints gbc_lblRegister = new GridBagConstraints();
-		gbc_lblRegister.insets = new Insets(0, 0, 5, 5);
-		gbc_lblRegister.gridx = 1;
-		gbc_lblRegister.gridy = 10;
+		GridBagConstraints gbc_lblRegister = Globals.gbc(1,10);
 		frmLogin.getContentPane().add(lblRegister, gbc_lblRegister);
 		
 		JButton btnRegister = new JButton("Register");
@@ -163,10 +141,7 @@ public class Login {
 			}
 		});
 		btnRegister.setFont(Globals.font);
-		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
-		gbc_btnRegister.insets = new Insets(0, 0, 5, 5);
-		gbc_btnRegister.gridx = 1;
-		gbc_btnRegister.gridy = 11;
+		GridBagConstraints gbc_btnRegister = Globals.gbc(1,11);
 		frmLogin.getContentPane().add(btnRegister, gbc_btnRegister);
 	}
 }
